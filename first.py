95da4c33a88d76ca469856729c3493472ef558d5
Denys Dashkevych <denysdashkevych@gmail.com>
2023-10-17 14:23:47 -0500

    class Coordinate

--- first.py
+++ first.py
@@ -790,7 +790,38 @@ try:
       normalize([0, 0, 0])
 except ZeroDivisionError:
       print('Invalid maximum element')
-      
+
+#%%
+
+class Coordinate(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
+    def __eq__(self):
+        return self.x == self.y
+        
+    def __repr__(self):
+        return self.x == self.y
+
+
+c1 = Coordinate(7, 8)
+c2 = Coordinate(7, 8)
+
+print(c1.eq())
+
 
 
 
