ab367d1e565a31da8676551f7d0c10dc35644b86
Denys Dashkevych <denysdashkevych@gmail.com>
2023-10-25 18:52:05 -0500

    Update first.py

--- first.py
+++ first.py
@@ -1055,10 +1055,61 @@ def program3(L):
 program3([]) 
         
 
+#%%
+
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        if L[i] > e:
+            return False
+    return False
+ 
+    
+
+def newsearch(L, e):
+    size = len(L)
+    for i in range(size):
+        if L[size-i-1] == e:
+            return True
+        if L[i] < e:
+            return False
+    return False
 
 
+print(newsearch([0,1,2,3,4,5,6,7,8,9], 2))
+
+#%%
 
 
+def swapSort(L): 
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(i+1, len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print(L)
+    print("Final L: ", L)
+
+
+def modSwapSort(L): 
+    """ L is a list on integers """
+    print("Original L: ", L)
+    for i in range(len(L)):
+        for j in range(len(L)):
+            if L[j] < L[i]:
+                # the next line is a short 
+                # form for swap L[i] and L[j]
+                L[j], L[i] = L[i], L[j] 
+                print(L)
+    print("Final L: ", L)
+    
+    
+
+swapSort([6,2,8,3,9,4,6,3,5])
 
 
 
